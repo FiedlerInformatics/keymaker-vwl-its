38f6050175945d838211082c2c4a1641dd355c2a
FiedlerInformatics <itsmarcel98@gmail.com>
2025-03-03 18:02:43 +0100

    login Window design vollständig

--- loginWindow.py
+++ loginWindow.py
@@ -1,4 +1,5 @@
 from keepassObject import Key
+from tkinter import *
 import tkinter as tk
 from tkinter import ttk
 import sv_ttk
@@ -10,37 +11,40 @@ login_window = tk.Tk()
 login_window.geometry("700x433")
 login_window.resizable(False,False)
 login_window.title("Keymaker")
-im = Image.open("loginImage.jpg")
-image = ImageTk.PhotoImage(im)
-image_label = tk.Label(login_window, image=image)
 
-sv_ttk.use_light_theme
+style = ttk.Style()
+style.theme_use("vista")
+login_window.iconbitmap(default="keymaker_images/lockSymbol.ico")
 
-keymakerHeader = tk.Label(login_window,text="Keymaker",font="Helvetica 40 bold")
-datenbankAuswahlMeldung = tk.Label(login_window, text="Wählen Sie eine .kdbx-Datei")
-error_message = tk.Label(login_window, text="")
+image = ImageTk.PhotoImage(Image.open("keymaker_images/loginImage2.jpg"))
+image_label = ttk.Label(login_window, image=image)
 
-browse_database = tk.Button(text=".kdbx-Datei")
-database_entry = tk.Entry(login_window, width=50)
+keymakerHeader = ttk.Label(login_window,text="Keymaker",font="Helvetica 40 bold")
+datenbankAuswahlMeldung = ttk.Label(login_window, text="Wählen Sie eine .kdbx-Datei", font="Helvetica")
+error_message = ttk.Label(login_window, text="")
 
-password_label = tk.Label(login_window, text="Password")
-password_entry = tk.Entry(login_window, show="●", width=50)
+browse_database = ttk.Button(text=".kdbx-Datei")
+database_entry = ttk.Entry(login_window, width=50)
 
-login_button = tk.Button(text="Anmelden", width=20, height=2)
+password_label = ttk.Label(login_window, text="Password", font="Helvetica 10")
+password_entry = ttk.Entry(login_window, show="●", width=50)
+
+login_button = ttk.Button(text="Anmelden", width=20)
 
 # Layout login_window
 
-
 image_label.place(x=0,y=0)
 keymakerHeader.place(x=355,y=40)
-datenbankAuswahlMeldung.place(x=400,y=144)
+datenbankAuswahlMeldung.place(x=380,y=144)
 error_message.place(x=400,y=180)
 
-browse_database.place(x=340,y=210)
+browse_database.place(x=340,y=215)
 database_entry.place(x=340,y=245)
 
-password_label.place(x=340,y=285)
-password_entry.place(x=340, y=305)
+password_label.place(x=340,y=280)
+password_entry.place(x=340, y=302)
+
+login_button.place(x=430,y=357)
 
 # Aktualisierung der Eingabe
 login_window.mainloop()
