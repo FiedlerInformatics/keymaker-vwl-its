ccb0058351fb9d90d78ec0f70027c44eba28ff4d
FiedlerInformatics <itsmarcel98@gmail.com>
2025-04-16 09:10:15 +0200

    loginWindow öffnet mainWindow

--- loginWindow.py
+++ loginWindow.py
@@ -8,7 +8,8 @@ import pykeepass
 from pykeepass import PyKeePass
 from pathlib import Path
 from PIL import ImageTk, Image
-import os
+import subprocess
+import sys
 
 # Erstellung des Key-Obj
 keyObject = Key()
@@ -31,7 +32,7 @@ image_label = ttk.Label(login_window, image=image)
 keymakerHeader = ttk.Label(login_window,text="Keymaker",font="Helvetica 40 bold")
 datenbankAuswahlMeldung = ttk.Label(login_window, text="Wählen Sie eine .kdbx-Datei", font="Helvetica")
 
-error_message = ttk.Label(login_window, text="")
+error_message = tk.Label(login_window, text="")
 
 browse_database = ttk.Button(text=".kdbx-Datei")
 database_entry = ttk.Entry(login_window, width=50)
@@ -46,7 +47,7 @@ login_button = ttk.Button(text="Anmelden", width=20)
 image_label.place(x=0,y=0)
 keymakerHeader.place(x=365,y=40)
 datenbankAuswahlMeldung.place(x=365,y=144)
-error_message.place(x=400,y=180)
+error_message.place(x=440,y=185)
 
 browse_database.place(x=340,y=215)
 database_entry.place(x=340,y=245)
@@ -61,27 +62,38 @@ def getDataBase() -> None:
     if keyObject.database_path:
         database_entry.delete(0,tk.END)
         database_entry.insert(0,keyObject.database_path)
+        print(keyObject.database_path)
 
 def check_file_type(filepath, extension) -> bool:
     return Path(filepath).suffix.lower() == extension.lower()
+
+def check_password() -> bool:
+    try: keyObject.database = PyKeePass(database_entry.get(), password=password_entry.get())
+    except pykeepass.exceptions.CredentialsError:
+        return False
+    return True
 
 def login() -> None:
     if len(keyObject.database_path) == 0:
         error_message.config(text="Es wurde keine Datei ausgewählt", fg="red")
         print(keyObject.database_path)
-    elif check_file_type(keyObject.database_path, "kdbx"):
-        error_message.config(text="Die ausgewählte Datei ist keine .kdbx Datenbank", fg="red")
-        print(database_entry.get())
     else:
-        try:
-            keyObject.database = PyKeePass(database_entry.get(), password=password_entry.get())
-        except pykeepass.exceptions.CredentialsError as credentialerror:
-            print(credentialerror)
+        if not check_password():
             error_message.config(text="Falsches Passwort", fg="red")
-        except FileNotFoundError as filenotfounderror:
-            print(filenotfounderror)
-            error_message.configure(text="Diese Datei existiert nicht", fg="red")
-        
+            return
+        else:       
+            keyObject.password = password_entry.get()
+            keyObject.database_path = database_entry.get()
+            keyObject.database = PyKeePass(database_entry.get(), password=password_entry.get())
+            open_mainWindow()
+
+def open_mainWindow() -> None:
+    subprocess.Popen(
+        [sys.executable, "mainWindow.py"] ,
+        creationflags=subprocess.CREATE_NO_WINDOW
+    )
+    login_window.destroy()
+
 browse_database.configure(command=getDataBase)
 login_button.configure(command=login)
 
--- mainWindow.py
+++ mainWindow.py
@@ -8,9 +8,16 @@ import pykeepass
 from pykeepass import PyKeePass
 from pathlib import Path
 from PIL import ImageTk, Image
+from keepassObject import Key
+import subprocess
+import pickle
+import os
+
+
 
 main_window = tk.Tk()
 main_window.geometry("900x700")
+main_window.minsize(900,700)
 main_window.title("Keymaker")
 
 style = ttk.Style(main_window)
@@ -19,26 +26,82 @@ main_window.iconbitmap(default="keymaker_images/lockSymbol.ico")
 
 trennlinie = tk.Frame(main_window,bg='black',width=1)
 
-create_key_buttton = ttk.Label(main_window,text="Key erstellen",font="Helvetica 17")
-print_key_buttton = ttk.Label(main_window,text="Key drucken", font="Helvetica 17")
+browse_txt = ttk.Button(main_window, text=".txt-Datei")
+txt_entry = ttk.Entry(main_window)
 
-gerat_label = ttk.Label(main_window,text="Gerät", font="Helvetica 15 bold")
-gerat_input = ttk.Entry(main_window,width=50,font=("Helvetica 15"))
+create_key_buttton = ttk.Label(main_window,text="Key erstellen",font="Helvetica 12 bold")
+print_key_buttton = ttk.Label(main_window,text="Key drucken", font="Helvetica 12 bold")
 
-# Layout main_window
-main_window.columnconfigure(0, weight=1)
-for col in (1, 2):
-    main_window.columnconfigure(col, weight=4)
+person_label = ttk.Label(main_window,text="Person", font="Helvetica 12")
+person_input = ttk.Entry(main_window, font=("Helvetica 12"))
+
+geraet_label = ttk.Label(main_window,text="Gerät", font="Helvetica 12")
+geraet_input = ttk.Entry(main_window,font=("Helvetica 12"))
+
+lehrstuhl_label = ttk.Label(main_window, text="Lehrstuhl", font="Helvetica 12")
+lehrstuhl_input = ttk.Entry(main_window, font=("Helvetica 12"))
+
+seriennummer_label = ttk.Label(main_window, text="Seriennummer", font="Helvetica 12")
+seriennummer_input = ttk.Entry(main_window, font=("Helvetica 12"))
+
+datum_label = ttk.Label(main_window, text="Datum", font="Helvetica 12")
+datum_input = ttk.Entry(main_window, font=("Helvetica 12"))
+
+inventarnummer_label = ttk.Label(main_window, text="Inventarnummer", font="Helvetica 12 ")
+inventarnummer_input = ttk.Entry(main_window, font=("Helvetica 12"))
+
+hilfskraft_label = ttk.Label(main_window, text="Hilfskraft", font="Helvetica 12")
+hilfskraft_input = ttk.Entry(main_window, font=("Helvetica 12"))
+
+bitlocker_key_label = ttk.Label(main_window, text="Bitlocker Key", font="Helvetica 12")
+bitlocker_key_input = ttk.Entry(main_window, font=("Helvetica 12"))
+
+bitlocker_bezeichner_label = ttk.Label(main_window, text="Bitlocker Bezeichner", font="Helvetica 12")
+bitlocker_bezeichner_input = ttk.Entry(main_window, font=("Helvetica 12"))
+
+
+main_window.columnconfigure(0,weight=1)
+main_window.columnconfigure(1,weight=1)
+main_window.columnconfigure(2,weight=1)
+main_window.columnconfigure(4,weight=1)
+for col in (1, 3):
+    main_window.columnconfigure(col, weight=3)
 for row in range(16):
     main_window.rowconfigure(row, weight=1)
 
 
-create_key_buttton.grid(column=0,row=1,sticky='w',padx=40)
-print_key_buttton.grid(column=0,row=2,sticky='w',padx=40)
+create_key_buttton.grid(column=0,row=1,sticky='w',padx=(10,5))
+print_key_buttton.grid(column=0,row=2,sticky='w',padx=(10,5))
 
 trennlinie.grid(row=0,column=0,sticky="nes",rowspan=16,padx=10)
 
-gerat_label.grid(row=1,column=1,sticky='wn')
-gerat_input.grid(row=1,column=1,sticky='ws')
+person_label.grid(row=2,column=1,sticky='wn',padx=(20,0))
+person_input.grid(row=2,column=1,sticky='wes',padx=(20,0))
+
+geraet_label.grid(row=2,column=3,sticky='wn',padx=(0,20))
+geraet_input.grid(row=2,column=3,sticky='wes',padx=(0,20))  
+
+lehrstuhl_label.grid(row=4, column=1, sticky='wn',padx=(20,0))
+lehrstuhl_input.grid(row=4, column=1, sticky='wes',padx=(20,0))
+
+seriennummer_label.grid(row=4, column=3, sticky='wn',padx=(0,20))
+seriennummer_input.grid(row=4, column=3, sticky='wes',padx=(0,20))
+
+datum_label.grid(row=6, column=1, sticky='wn',padx=(20,0))
+datum_input.grid(row=6, column=1, sticky='wes', padx=(20,0))
+
+inventarnummer_label.grid(row=6, column=3, sticky='wn',padx=(0,20))
+inventarnummer_input.grid(row=6, column=3, sticky='wes', padx=(0,20))
+
+hilfskraft_label.grid(row=8, column=1, sticky='wn',padx=(20,0))
+hilfskraft_input.grid(row=8, column=1, sticky='wes', padx=(20,0))
+
+bitlocker_bezeichner_label.grid(row=10, column=1, sticky='wn', padx=(20,20))
+bitlocker_bezeichner_input.grid(row=10, column=1, sticky='wes', columnspan=3, padx=(20,20))
+
+bitlocker_key_label.grid(row=12, column=1, sticky='wn',padx=(20,20 ))
+bitlocker_key_input.grid(row=12, column=1, sticky='wes', columnspan=3, padx=(20,20))
 
 main_window.mainloop()
+
+    
